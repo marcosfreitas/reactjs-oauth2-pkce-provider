5f9419319274a1a7b1d651195b22d9dc65f43faf
Marcos Freitas <marcosvsfreitas@gmail.com>
2022-11-09 19:22:49 -0300

    :construction: removed wrong declaration of deleted pkce class

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,3 @@ export { AuthService } from './domain/services/AuthService';
 export type { AuthServiceProps } from './domain/contracts/AuthServiceProps';
 export { AuthProvider } from './application/AuthProvider';
 export { AuthContext, useAuth } from './application/AuthContext';
-export { PKCE } from './shared/PKCE';
